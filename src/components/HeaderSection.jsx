d9b1ed8ddcd356ff6152779297577e54aab2175e
habiburrahman0540 <habiburrahman0540@gmail.com>
2024-09-16 01:22:24 +0600

    Fixed typing error

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Typed from 'typed.js';
 
 const HeaderSection = () => {
+
+  useEffect(() => {
+    const typed_strings = 'Web Designer, Web Developer, Front End Developer, WordPress Theme Development and Customization, Shopify Theme Developer, Shopify Custom React App Developer';
+    const typed = new Typed('.header h2', {
+      strings: typed_strings.split(', '),
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true,
+    });
+
+    // Clean up the Typed instance on component unmount
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <>
-        <div className="header" id="header">
-    <div className="content-inner">
-        <p>I'm</p>
-        <h1>Habibur Rahman</h1>
-        <h2></h2>
-        <div className="typed-text">Web Designer, Web Developer, Front End Developer, Apps Developer, Graphic Designer</div>
-    </div>
-</div>
-  <div className="large-btn">
-  <div className="content-inner">
-      <a className="btn" href="#"><i className="fa fa-download"></i>Resume</a>
-      <a className="btn" href="#"><i className="fa fa-hands-helping"></i>Hire Me On Upwork</a>
-  </div>
-</div>
+      <div className="header" id="header">
+        <div className="content-inner">
+          <p>I'm</p>
+          <h1>Habibur Rahman</h1>
+          <h2></h2>
+          <div className="typed-text">Web Designer, Web Developer, Front End Developer, Apps Developer, Graphic Designer</div>
+        </div>
+      </div>
+      <div className="large-btn">
+        <div className="content-inner">
+          <a className="btn" href="#"><i className="fa fa-download"></i>Resume</a>
+          <a className="btn" href="#"><i className="fa fa-hands-helping"></i>Hire Me On Upwork</a>
+        </div>
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default HeaderSection
+export default HeaderSection;
