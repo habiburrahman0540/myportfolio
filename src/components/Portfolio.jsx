85ae75857e023b4b2dbb6dcd1f5420c3cac9cc3e
habiburrahman0540 <habiburrahman0540@gmail.com>
2024-09-19 10:32:15 +0600

    added project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,12 +6,13 @@ const Portfolio = () => {
   const [filter, setFilter] = useState('*');
 
   const portfolioItems = [
-    { id: 1, category: 'wordpress', image: 'img/portfolio-1.jpg', title: 'Project Name', type: 'wordpress',urlLink:'#' },
-    { id: 2, category: 'shopify', image: 'img/portfolio-3.jpg', title: 'Project Name', type: 'shopify',urlLink:'#' },
-    { id: 3, category: 'wordpress', image: 'img/portfolio-5.jpg', title: 'Project Name', type: 'wordpress',urlLink:'#' },
+    { id: 1, category: 'wordpress', image: 'https://res.cloudinary.com/upwork-cloud/video/upload/c_scale,w_1000/v1686677809/catalog/1668671518582566912/rkb1tusg4l3dhe0xkfw8.WEBP', title: 'pixpoker blog site', type: 'wordpress',urlLink:'#' },
+    { id: 2, category: 'shopify', image: 'img/shopify-1.jpeg', title: 'Quickfresh', type: 'shopify',urlLink:'https://quickfresh-sandbox.myshopify.com/' },
+    { id: 3, category: 'wordpress', image: 'https://upwork-usw2-prod-agora-file-storage.s3.us-west-2.amazonaws.com/profile/portfolio/thumbnail/e50ab19448a512c9a77ad4e137087e12?response-content-disposition=inline;+filename=%22image_original%22;+filename*=utf-8%27%27image_original&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEBwaCXVzLXdlc3QtMiJGMEQCIAVG/OISWW%2BVgqAOrOiLqrzdPsJ9eQj1WLu3ukRShNBcAiBrIw8x0sLGMTePatFudhPmDo0eMBLG8sURaBCp8AZSNCrNBAhVEAAaDDczOTkzOTE3MzgxOSIMq1iwB8OzvG/%2BM/WMKqoEVk7BAWLkYqWryQmUmfXGSFibcqpunW5xhVbZM7vhzD%2B35MnDy5iigexIAsSNZl%2BW1Td4PCUwS5IePqSVoqy6Ptx9hU4gg1UeIJjYyv0mZCU4pPP5RiMCYmiTHG8lsV55nHQuJZb0MaocRObG6upvSEQx1mngtdmTjFDkpvr56RWb%2BX7yDNlvazwK/U3cyMld001Ks6VfQr4bPfQKwEJrHUL4O2w7KzIO46SCllzrqebrgE9mgF%2BXE2JlFpEdRA%2B7fsCZQiThFv9q7LNyOntdpqIH1yFQ7KoqHeil7fOsZyXqocHhb%2B9e4JLQFmdQZ3l5yDpWzDp0YZdhtyQwWw3ziilyJaEqzZ/3A8FOLHIM%2BqtAotWJD3J2Z5CrlQfJfNOAr/spkwZ6hWvE172lCI58Rrw2cPSUlTLsl1laXCigtoK2rR%2BErXHDYUYDuDGBwjnmH2KjF2zHeYA9AcEI0F/c6UlI2F5knVdpeEgtIosS8kxWuU1uEpHJO/mMinD7C%2BilQewLzVgTGbNDZRG9NVJJf9KM3Mgz4KvIHx80N1Sj8dZpqxaiwUi5Bz6IH4EKoESqk7nzum5UuyUZmzCRRYZfDy2qGRAt6DGec45kg6rYA2b5tJdW5wCX%2BcWQZcFmu%2BAVtDhVoP5KbjpGu2%2BSKCxT/l3rgNoRhm2fyB4l3PN31jU7j%2BJxv74xtuEKhR0Ch2BsKkBTQsFhpk8c6CDMQmUN4De6GgZnAerXr98wgriutwY6qAGvTrX6KU76tl6LNBrAcetLwqf5clyFhbIdmrxUiymMggkYmaHnHWh4tsrIP3M32GqpViXEOB0bbX5pHW44STYjRa3kl79qLC7f6CU1AyH5l9c%2B%2BdGNsQbeOgTDyviuP8sTtP%2BrgzlIZLXfDd5LZRY/d/5d1PwlAwsbPfNlIcrn4aHMSHGkHVESAL25Tf1JL5sBMOcej2OQpLZ2HZM6NATtJWm7VY7dCfw=&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240919T041712Z&X-Amz-SignedHeaders=host&X-Amz-Expires=900&X-Amz-Credential=ASIA2YR6PYW5XSU6HTSR/20240919/us-west-2/s3/aws4_request&X-Amz-Signature=450d787b0ebeba3267847fb933497ed3da031bec60ecc5632f80fa5aa27a70c4', title: 'Easypress.ai', type: 'wordpress',urlLink:'easypress.ai' },
     { id: 4, category: 'react', image: 'img/react-1.jpg', title: 'Nutrition Calculator', type: 'react' ,urlLink:'https://github.com/justmeats/nutrition-calculator'},
     { id: 5, category: 'react', image: 'img/react-2.jpeg', title: 'Justmeat -Shopify project ', type: 'react',urlLink:'https://www.justmeats.com/' },
     { id: 6, category: 'shopify', image: 'img/react-2.jpeg', title: 'Justmeat -Shopify project ', type: 'shopify',urlLink:'https://www.justmeats.com/' },
+    { id: 7, category: 'shopify', image: 'img/feastbox.jpeg', title: 'Feastbox -Shopify and React ', type: 'shopify',urlLink:'https://www.justmeats.com/' },
     // Add more items here
   ];
 
