8e5b8d5bf7dfbb94cde0521a5066eafc4d5ac7ce
Habibur Rahman <59167744+habiburrahman0540@users.noreply.github.com>
2025-09-06 20:41:47 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <div className="content-inner">
         <div className="row align-items-center">
             <div className="col-md-6">
-                <p>&copy; Copyright <span id="year"></span><a href="">2025</a>. All Rights Reserved</p>
+                <p>&copy; Copyright <span id="year"></span><a href="https://www.fiverr.com/mdhabib058">2025</a>. All Rights Reserved</p>
             </div>
             <div className="col-md-6">
-                <p>Design and Developed by <a href="">Habibur Rahman</a></p>
+                <p>Design and Developed by <a href="https://www.fiverr.com/mdhabib058" target="_blank">Habibur Rahman</a></p>
             </div>
         </div>
     </div>
