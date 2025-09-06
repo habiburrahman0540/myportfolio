9b3e92e604957078b3abdf22d9168a120607f832
Habibur Rahman <59167744+habiburrahman0540@users.noreply.github.com>
2025-09-06 20:45:18 +0600

    Update ContactMe.jsx

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -14,13 +14,13 @@ const ContactMe = () => {
                 <div className="contact-info">
                     <p><i className="fa fa-user"></i>Habibur Rahman</p>
                     <p><i className="fa fa-tag"></i>Web Designer & Developer</p>
-                    <p><i className="fa fa-envelope"></i><a href="mailto:habiburrahman06000@gmail.com">habiburrahman06000@gmail.com</a></p>
+                    <p><i className="fa fa-envelope"></i><a href="mailto:habiburrahman06000@gmail.com" target="_blank">habiburrahman06000@gmail.com</a></p>
                     <p><i className="fa fa-phone"></i><a href="tel:+8801913626951">+8801913626951</a></p>
                     <p><i className="fa fa-map-marker"></i>Holding No. 700, Rustompur, Faridgonj, Chandpur, Bangladesh</p>
                     <div className="social">
-                        <a className="btn" href="https://x.com/devhabibur1"><i className="fab fa-twitter"></i></a>
-                        <a className="btn" href="https://web.facebook.com/habiburrahman7"><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn" href="https://www.linkedin.com/in/habibur-rahman"><i className="fab fa-linkedin-in"></i></a>
+                        <a className="btn" href="https://x.com/devhabibur1" target="_blank"><i className="fab fa-twitter"></i></a>
+                        <a className="btn" href="https://web.facebook.com/habiburrahman7" target="_blank"><i className="fab fa-facebook-f"></i></a>
+                        <a className="btn" href="https://www.linkedin.com/in/habibur-rahman" target="_blank"><i className="fab fa-linkedin-in"></i></a>
                     </div>
                 </div>
             </div>
